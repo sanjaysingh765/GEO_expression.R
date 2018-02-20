5c69fbce5d69d7df52950f1bd370dfb3d10a26b0
sanjaysingh765 <sanjaysingh765@users.noreply.github.com>
2018-02-20 13:26:36 -0500

    Update GEO_expression.R

--- GEO_expression.R
+++ GEO_expression.R
@@ -3,12 +3,12 @@ library(GEOquery)
 options('download.file.method.GEOquery' = 'libcurl')
 
 # load series and platform data from GEO
-gset <- getGEO("GSE16765", GSEMatrix =TRUE, AnnotGPL=TRUE, destdir = "./")
-R.utils::gunzip("GSE16765_series_matrix.txt.gz") #change name of file
+gset <- getGEO("GSE16765", GSEMatrix =TRUE, AnnotGPL=TRUE, destdir = "./")  #change the repository accession id and directory
+#R.utils::gunzip("GSE16765_series_matrix.txt.gz") #change name of file
 gse <- gset[[1]]
 pd <- pData(gse)
 names(pd)
 head(pd)
 expressionTable =  exprs(gse)
 expressionTable
-write.table(expressionTable,sep="\t",row.names=T,file='expression.txt')
+write.table(expressionTable,sep="\t",row.names=T,file='expression.txt',quote=F)
