b19049d09206800a4b2feb83f2feb0e6d74fc01d
sanjaysingh765 <sanjaysingh765@users.noreply.github.com>
2018-03-01 16:43:55 -0500

    Update GEO_expression.R

--- GEO_expression.R
+++ GEO_expression.R
@@ -1,9 +1,27 @@
+#This script uses accession number as command
+#Uses Rscript script_name accession mnumber
+
+##First read in the arguments listed at the command line
+args=(commandArgs(TRUE))
+
+
+
+##args is now a list of character vectors
+## First check to see if arguments are passed.
+## Then cycle through each element of the list and evaluate the expressions.
+if(length(args)==0){
+    print("Please give a valid accession number such as GSE16765")
+}else{
 library(GEOquery)
 #options('download.file.method'='curl')
 options('download.file.method.GEOquery' = 'libcurl')
 
+
+##First read in the arguments listed at the command line
+args=(commandArgs(TRUE))
+
 # load series and platform data from GEO
-gset <- getGEO("GSE16765", GSEMatrix =TRUE, AnnotGPL=TRUE, destdir = "./")  #change the repository accession id and directory
+gset <- getGEO(args, GSEMatrix =TRUE, AnnotGPL=TRUE, destdir = "./")  #change the repository accession id and directory
 #R.utils::gunzip("GSE16765_series_matrix.txt.gz") #change name of file
 gse <- gset[[1]]
 pd <- pData(gse)
@@ -12,3 +30,4 @@ head(pd)
 expressionTable =  exprs(gse)
 expressionTable
 write.table(expressionTable,sep="\t",row.names=T,file='expression.txt',quote=F)
+}
